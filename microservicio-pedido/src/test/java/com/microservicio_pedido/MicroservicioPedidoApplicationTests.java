b421a1482b5b249d6c2ab6c7b66d32b21f667ccf
axelmaya69 <axeldbmaya96@gmail.com>
2025-05-26 22:45:25 -0600

    Agregando archivos en msvc-eureka

---
+++ microservicio-pedido/src/test/java/com/microservicio_pedido/MicroservicioPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio_pedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
