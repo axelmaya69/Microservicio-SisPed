067829eb23afc7ab959c2f9eec2364e39452983a
axelmaya69 <axeldbmaya96@gmail.com>
2025-05-26 22:44:52 -0600

    Agregando archivos en msvc-service

---
+++ microservicio-config-server/src/main/java/com/microservicio_config_server/MicroservicioConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.microservicio_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioConfigServerApplication.class, args);
+	}
+
+}
---
+++ microservicio-config-server/src/test/java/com/microservicio_config_server/MicroservicioConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
