e7bf1f1a9956310d897ca9a2481e00bbf8d59644
axelmaya69 <axeldbmaya96@gmail.com>
2025-05-27 21:31:38 -0600

    Agregando

--- microservicio-cliente/src/main/java/com/microservicio_cliente/MicroservicioClienteApplication.java
+++ microservicio-cliente/src/main/java/com/microservicio_cliente/MicroservicioClienteApplication.java
@@ -2,7 +2,9 @@ package com.microservicio_cliente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroservicioClienteApplication {
 
