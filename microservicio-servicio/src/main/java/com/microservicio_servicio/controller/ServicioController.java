016b4827eeab762e494ad5a743787b5394cdd978
axelmaya69 <axeldbmaya96@gmail.com>
2025-05-27 21:20:07 -0600

    Agregando

---
+++ microservicio-servicio/src/main/java/com/microservicio_servicio/controller/ServicioController.java
@@ -0,0 +1,30 @@
+package com.microservicio_servicio.controller;
+
+import com.microservicio_servicio.entity.Servicio;
+import com.microservicio_servicio.service.IServiceServ;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/servicio")
+public class ServicioController {
+
+    @Autowired
+    IServiceServ serviceServ;
+
+
+    @GetMapping("/get")
+    public ResponseEntity<?> obtenerTodos(){
+        List<Servicio> servicios = serviceServ.obtenerTodosServicios();
+        return ResponseEntity.ok(servicios);
+    }
+
+    
+
+}
