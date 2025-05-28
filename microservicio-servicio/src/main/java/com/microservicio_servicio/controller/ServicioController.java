ccf4b39b878e670b4861684988d5a84a395e28dc
axelmaya69 <axeldbmaya96@gmail.com>
2025-05-27 21:22:27 -0600

    Agregando

--- microservicio-servicio/src/main/java/com/microservicio_servicio/controller/ServicioController.java
+++ microservicio-servicio/src/main/java/com/microservicio_servicio/controller/ServicioController.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +26,11 @@ public class ServicioController {
         return ResponseEntity.ok(servicios);
     }
 
-    
+    @GetMapping("/get/{idServicio}")
+    public ResponseEntity<?> obtenerId(@PathVariable int idServicio){
+        Servicio unServicio = serviceServ.obtenerServicio(idServicio);
+        return ResponseEntity.ok(unServicio);
+    }
 
+    
 }
