d985ef551847ab8cd3b14e93e14a0ce8aecd9c75
axelmaya69 <axeldbmaya96@gmail.com>
2025-05-29 21:58:06 -0600

    Agregando

--- microservicio-pedido/src/main/java/com/microservicio_pedido/service/PedidoServiceImplementation.java
+++ microservicio-pedido/src/main/java/com/microservicio_pedido/service/PedidoServiceImplementation.java
@@ -1,7 +1,10 @@
 package com.microservicio_pedido.service;
 
 
+import com.microservicio_pedido.cliente.IClientePedido;
+import com.microservicio_pedido.controller.DTO.PedidoDTO;
 import com.microservicio_pedido.entity.Pedido;
+import com.microservicio_pedido.http.response.ClienteByPedidoResponse;
 import com.microservicio_pedido.repository.IPedido;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +17,8 @@ public class PedidoServiceImplementation implements IPedidoService{
     @Autowired
     private IPedido iPedido;
 
+    @Autowired
+    private IClientePedido clientePedido;
 
     @Override
     public Pedido crearPedido(Pedido pedido) {
@@ -29,6 +34,20 @@ public class PedidoServiceImplementation implements IPedidoService{
     public List<Pedido> obtenerPedidos() {
         return iPedido.findAll();
     }
+/*
+    @Override
+    public ClienteByPedidoResponse findClientesByIdPedidos(int id) {
+        Pedido pedido = iPedido.findById(id).orElse(new Pedido());
+
+        List<PedidoDTO> clienteDTOSs = clientePedido.findAllClientsByServicio(id);
+        return ClienteByPedidoResponse.builder()
+                .clienteId(pedido.getClienteId())
+                .fecha(pedido.getFecha())
+                .productos(pedido.getProducos())
+                .clienteDTOS(clienteDTOSs)
+                .build();
+    }
+ */
 
     @Override
     public Pedido editarPedido(int idPedido, Pedido pedido) {
