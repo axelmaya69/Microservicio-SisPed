105bde71f87fc2f1a76a4dee2204726fe0428dcd
axelmaya69 <axeldbmaya96@gmail.com>
2025-05-30 22:10:15 -0600

    Agregando

--- microservicio-pedido/src/main/java/com/microservicio_pedido/controller/PedidoProductoController.java
+++ microservicio-pedido/src/main/java/com/microservicio_pedido/controller/PedidoProductoController.java
@@ -1,8 +1,10 @@
 package com.microservicio_pedido.controller;
 
+import com.microservicio_pedido.controller.DTO.PedidoProductoDTO;
 import com.microservicio_pedido.entity.PedidoProducto;
 import com.microservicio_pedido.service.IPedidoProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,11 +27,17 @@ public class PedidoProductoController {
         PedidoProducto obtenerPed = pedidoProductoService.obtenerPedidoProducto(idPedidoProd);
         return ResponseEntity.ok(obtenerPed);
     }
-
+/*
     @PostMapping("/post")
     public ResponseEntity<?> crearPedidoProd(@RequestBody PedidoProducto pedidoProducto){
         PedidoProducto crearPedidoProducto = pedidoProductoService.crearPedidoProducto(pedidoProducto);
         return ResponseEntity.ok(crearPedidoProducto);
+    }
+ */
+    @PostMapping("/post")
+    public ResponseEntity<PedidoProductoDTO> agregarProducto(@RequestBody PedidoProductoDTO dto){
+        PedidoProductoDTO nuevo = pedidoProductoService.crearPedido(dto);
+        return new ResponseEntity<>(nuevo, HttpStatus.CREATED);
     }
 
     @PutMapping("/put/{idPedidoProd}")
