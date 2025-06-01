58c44b5cc501d0c7adb54850988836be3c5a4bb2
axelmaya69 <axeldbmaya96@gmail.com>
2025-05-31 21:27:06 -0600

    Agregando

--- microservicio-pedido/src/main/java/com/microservicio_pedido/service/PedidoServiceImplementation.java
+++ microservicio-pedido/src/main/java/com/microservicio_pedido/service/PedidoServiceImplementation.java
@@ -2,13 +2,17 @@ package com.microservicio_pedido.service;
 
 
 import com.microservicio_pedido.cliente.IClientePedido;
+import com.microservicio_pedido.cliente.IClientePedidoProducto;
+import com.microservicio_pedido.controller.DTO.CrearPedidoDTO;
 import com.microservicio_pedido.controller.DTO.PedidoDTO;
+import com.microservicio_pedido.controller.DTO.PedidoDetalleDTO;
+import com.microservicio_pedido.controller.DTO.PedidoProductoDTO;
 import com.microservicio_pedido.entity.Pedido;
-import com.microservicio_pedido.http.response.ClienteByPedidoResponse;
 import com.microservicio_pedido.repository.IPedido;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -20,10 +24,43 @@ public class PedidoServiceImplementation implements IPedidoService{
     @Autowired
     private IClientePedido clientePedido;
 
+    @Autowired
+    IClientePedidoProducto clientePedidoProducto;
+
+    /*
     @Override
     public Pedido crearPedido(Pedido pedido) {
         return iPedido.save(pedido);
     }
+     */
+        @Override
+    public PedidoDTO crearPedido(CrearPedidoDTO dto){
+        Pedido pedido = new Pedido();
+        pedido.setClienteId(dto.getIdCliente());
+        pedido.setFecha(LocalDate.now());
+        pedido = iPedido.save(pedido);
+
+        return PedidoDTO.builder()
+                .idPedido(pedido.getId())
+                .idCliente(pedido.getClienteId())
+                .fecha(pedido.getFecha())
+                .build();
+    }
+
+
+    public PedidoDetalleDTO obtenerDetalle(int idPedido) {
+        Pedido pedido = iPedido.findById(idPedido)
+                .orElseThrow(() -> new RuntimeException("Pedido no encontrado"));
+
+        List<PedidoProductoDTO> productos = clientePedidoProducto.obtenerPorPedido(idPedido);
+
+        return PedidoDetalleDTO.builder()
+                .idPedido(pedido.getId())
+                .fecha(pedido.getFecha())
+                .productos(productos)
+                .build();
+    }
+
 
     @Override
     public Pedido obtenerPedido(int idPedido) {
