fd79ee66ef181d9a467072b181d107eeff7d0f0f
axelmaya69 <axeldbmaya96@gmail.com>
2025-06-01 22:54:17 -0600

    agregando

---
+++ microservicio-cliente/src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO cliente (nombre, apellido_p, apellido_m, email) VALUES
+('Juan', 'Pérez', 'Gómez', 'juan.perez@example.com'),
+('María', 'López', 'Ramírez', 'maria.lopez@example.com'),
+('Carlos', 'Hernández', 'Martínez', 'carlos.hernandez@example.com'),
+('Ana', 'García', 'Ruiz', 'ana.garcia@example.com'),
+('Luis', 'Sánchez', 'Torres', 'luis.sanchez@example.com'),
+('Laura', 'Díaz', 'Flores', 'laura.diaz@example.com'),
+('Jorge', 'Ramírez', 'Morales', 'jorge.ramirez@example.com'),
+('Lucía', 'Mendoza', 'Núñez', 'lucia.mendoza@example.com'),
+('Pedro', 'Ortega', 'Silva', 'pedro.ortega@example.com'),
+('Elena', 'Cruz', 'Fernández', 'elena.cruz@example.com');
