bce3fb7a08ca1900c20b756e5de27b7c4d04542f
axelmaya69 <axeldbmaya96@gmail.com>
2025-06-01 22:53:58 -0600

    agregando

--- microservicio-cliente/src/main/java/com/microservicio_cliente/entity/Cliente.java
+++ microservicio-cliente/src/main/java/com/microservicio_cliente/entity/Cliente.java
@@ -1,16 +1,14 @@
 package com.microservicio_cliente.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "cliente")
 public class Cliente {
 
     @Id
