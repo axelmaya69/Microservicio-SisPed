f8f14d3462be4f35ad6f48c65af711811d43e1ad
axelmaya69 <axeldbmaya96@gmail.com>
2025-06-02 22:32:22 -0600

    agregando

--- microservicio-cliente/src/main/resources/import.sql
+++ microservicio-cliente/src/main/resources/import.sql
@@ -1,11 +1,11 @@
-INSERT INTO cliente (nombre, apellido_p, apellido_m, email) VALUES
-('Juan', 'Pérez', 'Gómez', 'juan.perez@example.com'),
-('María', 'López', 'Ramírez', 'maria.lopez@example.com'),
-('Carlos', 'Hernández', 'Martínez', 'carlos.hernandez@example.com'),
-('Ana', 'García', 'Ruiz', 'ana.garcia@example.com'),
-('Luis', 'Sánchez', 'Torres', 'luis.sanchez@example.com'),
-('Laura', 'Díaz', 'Flores', 'laura.diaz@example.com'),
-('Jorge', 'Ramírez', 'Morales', 'jorge.ramirez@example.com'),
-('Lucía', 'Mendoza', 'Núñez', 'lucia.mendoza@example.com'),
-('Pedro', 'Ortega', 'Silva', 'pedro.ortega@example.com'),
-('Elena', 'Cruz', 'Fernández', 'elena.cruz@example.com');
+INSERT INTO cliente (nombre, apellidoP, apellidoM, email) VALUES('Juan', 'Pérez', 'Gómez', 'juan.perez@example.com');
+INSERT INTO cliente (nombre, apellidoP, apellidoM, email) VALUES('María', 'López', 'Ramírez', 'maria.lopez@example.com');
+INSERT INTO cliente (nombre, apellidoP, apellidoM, email) VALUES('Carlos', 'Hernández', 'Martínez', 'carlos.hernandez@example.com');
+INSERT INTO cliente (nombre, apellidoP, apellidoM, email) VALUES('Ana', 'García', 'Ruiz', 'ana.garcia@example.com');
+INSERT INTO cliente (nombre, apellidoP, apellidoM, email) VALUES('Luis', 'Sánchez', 'Torres', 'luis.sanchez@example.com');
+INSERT INTO cliente (nombre, apellidoP, apellidoM, email) VALUES('Laura', 'Díaz', 'Flores', 'laura.diaz@example.com');
+INSERT INTO cliente (nombre, apellidoP, apellidoM, email) VALUES('Jorge', 'Ramírez', 'Morales', 'jorge.ramirez@example.com');
+INSERT INTO cliente (nombre, apellidoP, apellidoM, email) VALUES('Lucía', 'Mendoza', 'Núñez', 'lucia.mendoza@example.com');
+INSERT INTO cliente (nombre, apellidoP, apellidoM, email) VALUES('Pedro', 'Ortega', 'Silva', 'pedro.ortega@example.com');
+INSERT INTO cliente (nombre, apellidoP, apellidoM, email) VALUES('Elena', 'Cruz', 'Fernández', 'elena.cruz@example.com');
+INSERT INTO cliente (nombre, apellidoP, apellidoM, email) VALUES('Axel Daniel', 'Bartolo', 'Maya', 'axeldanuel@example.com');
