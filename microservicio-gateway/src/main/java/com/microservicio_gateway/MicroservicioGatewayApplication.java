b5c52d86c8ac884d43832b265b3e1ba7615cba59
axelmaya69 <axeldbmaya96@gmail.com>
2025-06-03 22:24:23 -0600

    agregando

--- microservicio-gateway/src/main/java/com/microservicio_gateway/MicroservicioGatewayApplication.java
+++ microservicio-gateway/src/main/java/com/microservicio_gateway/MicroservicioGatewayApplication.java
@@ -3,6 +3,7 @@ package com.microservicio_gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MicroservicioGatewayApplication {
 
