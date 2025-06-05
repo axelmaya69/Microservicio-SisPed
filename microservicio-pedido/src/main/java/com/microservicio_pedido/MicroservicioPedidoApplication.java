8c9766b007717a583fb5ed6645ca2eafc1212763
axelmaya69 <axeldbmaya96@gmail.com>
2025-06-04 22:37:27 -0600

    agregando

--- microservicio-pedido/src/main/java/com/microservicio_pedido/MicroservicioPedidoApplication.java
+++ microservicio-pedido/src/main/java/com/microservicio_pedido/MicroservicioPedidoApplication.java
@@ -3,8 +3,9 @@ package com.microservicio_pedido;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MicroservicioPedidoApplication {
