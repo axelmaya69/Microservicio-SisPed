c7ff64db3546653d67f8c49693dfbb1ecc23f00f
axelmaya69 <axeldbmaya96@gmail.com>
2025-06-05 22:47:49 -0600

    agregando

--- microservicio-pedido/src/main/java/com/microservicio_pedido/service/PedidoProductoServiceImplementation.java
+++ microservicio-pedido/src/main/java/com/microservicio_pedido/service/PedidoProductoServiceImplementation.java
@@ -1,8 +1,5 @@
 package com.microservicio_pedido.service;
 
-import com.microservicio_pedido.controller.DTO.CrearPedidoDTO;
-import com.microservicio_pedido.controller.DTO.CrearPedidoProductoDTO;
-import com.microservicio_pedido.controller.DTO.PedidoProductoDTO;
 import com.microservicio_pedido.entity.PedidoProducto;
 import com.microservicio_pedido.repository.IPedidoProducto;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,25 +13,10 @@ public class PedidoProductoServiceImplementation implements IPedidoProductoServi
     @Autowired
     private IPedidoProducto IPP;
 
-    /*
+
     @Override
     public PedidoProducto crearPedidoProducto(PedidoProducto pedidoProducto) {
         return IPP.save(pedidoProducto);
-    }
-     */
-
-    @Override
-    public PedidoProductoDTO crearPedido(CrearPedidoProductoDTO dto) {
-        PedidoProducto pedidoProducto = new PedidoProducto();
-        pedidoProducto.setIdProducto(dto.getIdProducto());
-        pedidoProducto.setCantidad(dto.getCantidad());
-        pedidoProducto = IPP.save(pedidoProducto);
-
-        return PedidoProductoDTO.builder()
-                .id(pedidoProducto.getId())
-                .idPedido(pedidoProducto.getIdProducto())
-                .cantidad(pedidoProducto.getCantidad())
-                .build();
     }
 
     @Override
