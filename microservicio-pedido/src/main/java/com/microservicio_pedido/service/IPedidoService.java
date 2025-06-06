0f9b916dc3cbaf189a5aceb107006817cbeef4f5
axelmaya69 <axeldbmaya96@gmail.com>
2025-06-05 22:47:41 -0600

    agregando

--- microservicio-pedido/src/main/java/com/microservicio_pedido/service/IPedidoService.java
+++ microservicio-pedido/src/main/java/com/microservicio_pedido/service/IPedidoService.java
@@ -1,31 +1,16 @@
 package com.microservicio_pedido.service;
 
-import com.microservicio_pedido.controller.DTO.CrearPedidoDTO;
-import com.microservicio_pedido.controller.DTO.PedidoDTO;
-import com.microservicio_pedido.controller.DTO.PedidoDetalleDTO;
 import com.microservicio_pedido.entity.Pedido;
-import com.microservicio_pedido.http.response.ClienteByPedidoResponse;
 
 import java.util.List;
 
 public interface IPedidoService {
 
-    PedidoDTO crearPedido(CrearPedidoDTO dto);
-
-    /*
-    @Override
-    public Pedido crearPedido(Pedido pedido) {
-        return iPedido.save(pedido);
-    }
-     */
-
-    PedidoDetalleDTO obtenerDetalle(int idPedido);
+    Pedido crearPedido(Pedido pedido);
 
     Pedido obtenerPedido(int idPedido);
 
     List<Pedido> obtenerPedidos();
-
-    //ClienteByPedidoResponse findClientesByIdPedidos(int id);
 
     Pedido editarPedido(int idPedido, Pedido pedido);
 
