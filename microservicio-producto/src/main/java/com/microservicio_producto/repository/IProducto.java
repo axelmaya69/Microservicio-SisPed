6956143bb63fdc8272dfa11ccce4c9e639059a19
axelmaya69 <axeldbmaya96@gmail.com>
2025-06-08 22:44:07 -0600

    agregando

--- microservicio-producto/src/main/java/com/microservicio_producto/repository/IProducto.java
+++ microservicio-producto/src/main/java/com/microservicio_producto/repository/IProducto.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IServicio extends JpaRepository<Producto,Integer> {
+public interface IProducto extends JpaRepository<Producto,Integer> {
 }
