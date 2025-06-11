1c7e68cb9b6e0987af8e829226b3fc593ebfd29d
axelmaya69 <axeldbmaya96@gmail.com>
2025-06-10 22:54:18 -0600

    agregando

---
+++ microservicio-pedido/src/main/java/com/microservicio_pedido/http/responses/ProductoByPedidoProductoResponse.java
@@ -0,0 +1,26 @@
+package com.microservicio_pedido.http.responses;
+
+import com.microservicio_pedido.controller.DTO.ClienteDTO;
+import com.microservicio_pedido.controller.DTO.ProductoDTO;
+import com.microservicio_pedido.entity.Pedido;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class ProductoByPedidoProductoResponse {
+
+    private int idPedidoProducto;
+
+    private int idProducto;
+
+    private Integer cantidad;
+
+    private float precioUnitario;
+
+    private int idPedido;
+
+    private List<ProductoDTO> productoDTO;
+}
