ffc8dc5f034177e87123f1a9a1f5afb03b2dc89a
axelmaya69 <axeldbmaya96@gmail.com>
2025-06-10 22:53:50 -0600

    agregando

--- microservicio-pedido/src/main/java/com/microservicio_pedido/entity/PedidoProducto.java
+++ microservicio-pedido/src/main/java/com/microservicio_pedido/entity/PedidoProducto.java
@@ -25,6 +25,7 @@ public class PedidoProducto {
     private float precioUnitario;
 
     // Relación física interna con Pedido
+    @Getter
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "pedido_id", nullable = false)
     private Pedido pedido;
