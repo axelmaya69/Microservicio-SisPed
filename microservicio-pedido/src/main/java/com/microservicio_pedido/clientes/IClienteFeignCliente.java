cc064cab28f8b2c3c0e967214e93ffd06f173667
axelmaya69 <axeldbmaya96@gmail.com>
2025-06-12 22:30:15 -0600

    agregando

--- microservicio-pedido/src/main/java/com/microservicio_pedido/clientes/IClienteFeignCliente.java
+++ microservicio-pedido/src/main/java/com/microservicio_pedido/clientes/IClienteFeignCliente.java
@@ -2,15 +2,12 @@ package com.microservicio_pedido.clientes;
 
 
 import com.microservicio_pedido.controller.DTO.ClienteDTO;
-import com.microservicio_pedido.controller.DTO.ProductoDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
-
 @FeignClient(name = "microservicio-cliente",url = "http://localhost:8080/api/cliente")
-public interface ICliente {
+public interface IClienteFeignCliente {
 
     @GetMapping("get/{id}")
     ClienteDTO getClientById(@PathVariable int id);
