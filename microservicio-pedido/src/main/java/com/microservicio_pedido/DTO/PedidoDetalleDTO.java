62a08860583692890ceedeca64773db372669410
axelmaya69 <axeldbmaya96@gmail.com>
2025-06-14 22:53:25 -0600

    agregando

--- microservicio-pedido/src/main/java/com/microservicio_pedido/DTO/PedidoDetalleDTO.java
+++ microservicio-pedido/src/main/java/com/microservicio_pedido/DTO/PedidoDetalleDTO.java
@@ -1,11 +1,8 @@
-package com.microservicio_pedido.controller.DTO;
+package com.microservicio_pedido.DTO;
 
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @Getter
 @Setter
