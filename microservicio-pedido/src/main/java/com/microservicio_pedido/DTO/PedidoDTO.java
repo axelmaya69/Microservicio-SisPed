e818e1e00b4de0ee88048ee1fd7fa1758be0f004
axelmaya69 <axeldbmaya96@gmail.com>
2025-06-16 22:46:32 -0600

    agregando

--- microservicio-pedido/src/main/java/com/microservicio_pedido/DTO/PedidoDTO.java
+++ microservicio-pedido/src/main/java/com/microservicio_pedido/DTO/PedidoDTO.java
@@ -1,5 +1,6 @@
 package com.microservicio_pedido.DTO;
 
+import com.microservicio_pedido.entity.PedidoProducto;
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,12 +11,11 @@ import java.util.List;
 @Builder
 public class PedidoDTO{
 
-private int idPedido;
+    private int idPedido;
 
-private int idCliente;
+    private int idCliente;
 
-private LocalDate fecha;
+    private LocalDate fecha;
 
-private List<ProductoDTO> productos;
-
+    private List<PedidoProducto> productos;
 }
