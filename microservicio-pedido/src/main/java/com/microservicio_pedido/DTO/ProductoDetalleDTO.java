c487dcb2c32629cf1eb342ba029e5488e5346916
axelmaya69 <axeldbmaya96@gmail.com>
2025-06-17 22:29:19 -0600

    agregando

---
+++ microservicio-pedido/src/main/java/com/microservicio_pedido/DTO/ProductoDetalleDTO.java
@@ -0,0 +1,22 @@
+package com.microservicio_pedido.DTO;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class ProductoDetalleDTO {
+
+    private int idProducto;
+
+    private String nombre;
+
+    private float precio;
+
+    private int cantidad;
+
+    private float precioUnitario;
+
+}
