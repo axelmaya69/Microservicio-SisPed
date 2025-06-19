ddbb57feec51ea8cda50a303053cef8f2190dd9d
axelmaya69 <axeldbmaya96@gmail.com>
2025-06-18 22:12:11 -0600

    agregando

--- microservicio-pedido/src/main/java/com/microservicio_pedido/clientes/IProductoFeignCliente.java
+++ microservicio-pedido/src/main/java/com/microservicio_pedido/clientes/IProductoFeignCliente.java
@@ -5,9 +5,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+//Feing hacia el microservicio producto
 @FeignClient(name = "microservicio-producto",url = "http://localhost:8080/api/producto")
 public interface IProductoFeignCliente {
 
+    //Endpoint que devuelve un producto
     @GetMapping("/get/{id}")
     ProductoDTO getProductoById(@PathVariable int id);
 
