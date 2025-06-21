3f65cee67d7cdcc790405964af35b0c01f204307
axelmaya69 <axeldbmaya96@gmail.com>
2025-06-20 22:23:02 -0600

    agregando

--- microservicio-pedido/src/main/java/com/microservicio_pedido/entity/PedidoProducto.java
+++ microservicio-pedido/src/main/java/com/microservicio_pedido/entity/PedidoProducto.java
@@ -5,9 +5,6 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.cglib.core.Local;
-
-import java.time.LocalDate;
 
 @Entity
 @Getter
