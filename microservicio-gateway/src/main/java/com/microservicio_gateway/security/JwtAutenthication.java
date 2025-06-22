d3f323aac35795f2084ea6ff32469e9901ab0e2c
axelmaya69 <axeldbmaya96@gmail.com>
2025-06-21 22:28:26 -0600

    agregando security

--- microservicio-gateway/src/main/java/com/microservicio_gateway/security/JwtAutenthication.java
+++ microservicio-gateway/src/main/java/com/microservicio_gateway/security/JwtAutenthication.java
@@ -1,12 +1,28 @@
 package com.microservicio_gateway.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Configuration
 public class JwtAutenthication extends OncePerRequestFilter {
 
+    @Value("${jwt.secret}")
     private String secretKey;
 
-    
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String header = request.getHeader("Authorization");
+
+        if (header == null || !header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+
 }
