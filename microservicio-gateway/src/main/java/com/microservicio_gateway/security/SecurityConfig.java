7c56f6c6467b1f29fe252916c2452d91e6453e49
axelmaya69 <axeldbmaya96@gmail.com>
2025-06-21 22:25:15 -0600

    agregando security

--- microservicio-gateway/src/main/java/com/microservicio_gateway/security/SecurityConfig.java
+++ microservicio-gateway/src/main/java/com/microservicio_gateway/security/SecurityConfig.java
@@ -1,10 +1,23 @@
 package com.microservicio_gateway.security;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    JwtAutenthication jwtfilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()  // endpoints públicos
+                        .anyRequest().authenticated()
+                )
 }
