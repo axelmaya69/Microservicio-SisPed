38cfc72eedb469de58332816459a3ca1650150bf
axelmaya69 <axeldbmaya96@gmail.com>
2025-06-21 22:24:19 -0600

    agregando security

--- microservicio-gateway/src/main/java/com/microservicio_gateway/security/SecurityConfig.java
+++ microservicio-gateway/src/main/java/com/microservicio_gateway/security/SecurityConfig.java
@@ -1,4 +1,10 @@
 package com.microservicio_gateway.security;
 
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 }
