4d79040e4c01f387c6a3cf830f915143cb422fbf
axelmaya69 <axeldbmaya96@gmail.com>
2025-06-22 21:55:33 -0600

    agregando security

--- microservicio-gateway/src/main/java/com/microservicio_gateway/security/JwtAutenthication.java
+++ microservicio-gateway/src/main/java/com/microservicio_gateway/security/JwtAutenthication.java
@@ -1,5 +1,6 @@
 package com.microservicio_gateway.security;
 
+import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
