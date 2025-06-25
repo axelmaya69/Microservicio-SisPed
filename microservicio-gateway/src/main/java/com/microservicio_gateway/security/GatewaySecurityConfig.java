472a6788e7190280ec844494f35adda974a68dba
axelmaya69 <axeldbmaya96@gmail.com>
2025-06-24 21:44:02 -0600

    agregando security con oauth

--- microservicio-gateway/src/main/java/com/microservicio_gateway/security/GatewaySecurityConfig.java
+++ microservicio-gateway/src/main/java/com/microservicio_gateway/security/GatewaySecurityConfig.java
@@ -2,10 +2,13 @@ package com.microservicio_gateway.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import java.net.URI;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -15,9 +18,17 @@ public class GatewaySecurityConfig {
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
                 .authorizeExchange(exchange -> exchange
-                        .pathMatchers("/login", "/oauth2/**").permitAll()
-                        .pathMatchers("/api/**").authenticated() // protegidas
-                        .anyExchange().denyAll()
+                        .pathMatchers("/login", "/").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .logout(logout -> logout
+                        .logoutSuccessHandler((exchange, authentication) -> {
+                            exchange.getExchange().getResponse()
+                                    .setStatusCode(HttpStatus.FOUND);
+                            exchange.getExchange().getResponse().getHeaders()
+                                    .setLocation(URI.create("/inicio"));
+                            return exchange.getExchange().getResponse().setComplete();
+                        })
                 )
                 .oauth2Login(Customizer.withDefaults()) // reactivo
                 .build();
