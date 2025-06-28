fe479a8be17e6c4d6985e02563f0e17e4edd4375
axelmaya69 <axeldbmaya96@gmail.com>
2025-06-27 22:48:05 -0600

    comentario

--- microservicio-cliente/src/main/java/com/microservicio_cliente/MicroservicioClienteApplication.java
+++ microservicio-cliente/src/main/java/com/microservicio_cliente/MicroservicioClienteApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class MicroservicioClienteApplication {
+public class  MicroservicioClienteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroservicioClienteApplication.class, args);
