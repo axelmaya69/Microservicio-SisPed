674e51113267828204a5ff267f561de65d714052
axelmaya69 <axeldbmaya96@gmail.com>
2025-06-29 22:48:31 -0600

    agregando security config jwt

--- microservicio-gateway/src/main/java/com/microservicio_gateway/security/JwtAuthenticationFilter.java
+++ microservicio-gateway/src/main/java/com/microservicio_gateway/security/JwtAuthenticationFilter.java
@@ -1,4 +1,49 @@
 package com.microservicio_gateway.security;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.io.IOException;
+
 public class JwtAuthenticationFilter {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String header = request.getHeader("Authorization");
+
+        if (header == null || !header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = header.substring(7);
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            String username = claims.getSubject();
+
+            UsernamePasswordAuthenticationToken auth =
+                    new UsernamePasswordAuthenticationToken(username, null, List.of());
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        } catch (JwtException e) {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }
