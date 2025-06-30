924aefb4c0e1f89872b819923132bbd66feb7340
axelmaya69 <axeldbmaya96@gmail.com>
2025-06-29 22:44:44 -0600

    agregando security config jwt

--- microservicio-gateway/src/main/java/com/microservicio_gateway/security/SecurityConfig.java
+++ microservicio-gateway/src/main/java/com/microservicio_gateway/security/SecurityConfig.java
@@ -1,4 +1,18 @@
 package com.microservicio_gateway.security;
 
+import org.springframework.context.annotation.Bean;
+
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()  // endpoints públicos
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
 }
