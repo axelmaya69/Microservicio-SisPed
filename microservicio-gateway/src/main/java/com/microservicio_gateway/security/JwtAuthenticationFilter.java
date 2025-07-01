62aed2672c0fcabee9c3b1f0047d7e4383e3d966
axelmaya69 <axeldbmaya96@gmail.com>
2025-06-30 22:31:15 -0600

    agregando security config jwt

--- microservicio-gateway/src/main/java/com/microservicio_gateway/security/JwtAuthenticationFilter.java
+++ microservicio-gateway/src/main/java/com/microservicio_gateway/security/JwtAuthenticationFilter.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.io.IOException;
 import java.util.List;
