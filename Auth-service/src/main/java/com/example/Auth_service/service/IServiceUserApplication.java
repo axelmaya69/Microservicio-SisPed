edd9bf1195d455bbc7cab749ee4d761f1e3ee933
axelmaya69 <axeldbmaya96@gmail.com>
2025-07-04 22:12:02 -0600

    agregando security auth

--- Auth-service/src/main/java/com/example/Auth_service/service/IServiceUserApplication.java
+++ Auth-service/src/main/java/com/example/Auth_service/service/IServiceUserApplication.java
@@ -1,7 +1,9 @@
 package com.example.Auth_service.service;
 
 import com.example.Auth_service.entity.User;
+import com.example.Auth_service.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,19 +14,27 @@ import java.util.List;
 public class IServiceUserApplication implements  IServiceUser{
 
     @Autowired
-    IServiceUser serviceUser;
+    IUserRepository userRepository;
+
+
 
     @Autowired
     PasswordEncoder passwordEncoder;
 
+    @Value("${jwt.secret}")
+    private String secretKey;
+
     @Override
-    public User registrarUser(User user) {
-        return null;
+    public User registrarUser(String username, String password) {
+        String encodedPassword = passwordEncoder.encode(password);
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(encodedPassword);
+        return userRepository.save(user);
     }
 
     @Override
-    public String logearUser(long idUser) {
+    public String logearUser(String username, String password) {
         return "";
     }
-
 }
