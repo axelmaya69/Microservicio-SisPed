d33a1ec873ee25e5a010acde39cda26bda537e88
axelmaya69 <axeldbmaya96@gmail.com>
2025-07-06 22:31:28 -0600

    agregando security auth

---
+++ Auth-service/src/main/java/com/example/Auth_service/feigns/IClienteFeignClient.java
@@ -0,0 +1,15 @@
+package com.example.Auth_service.feigns;
+
+
+import com.example.Auth_service.dtos.ClienteRequest;
+import com.example.Auth_service.dtos.ClienteResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "microservicio-cliente")
+public interface IClienteFeignClient {
+
+    @PostMapping("/cliente")
+    ClienteResponse crearCliente(@RequestBody ClienteRequest cliente);
+}
