29bdc8153d996e3f58a6acf4ada39884f8c33b0c
axelmaya69 <axeldbmaya96@gmail.com>
2025-07-06 22:31:15 -0600

    agregando security auth

--- Auth-service/src/main/java/com/example/Auth_service/dtos/ClienteRequest.java
+++ Auth-service/src/main/java/com/example/Auth_service/dtos/ClienteRequest.java
@@ -1,4 +1,19 @@
 package com.example.Auth_service.dtos;
 
+import lombok.Data;
+
+@Data
 public class ClienteRequest {
+
+    private String nombre;
+
+    private String apellidoP;
+
+    private String apellidoM;
+
+    private String telefono;
+
+
+
+
 }
