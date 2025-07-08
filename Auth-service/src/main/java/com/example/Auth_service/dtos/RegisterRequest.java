cb208b294daacd1cc490c27269ba02b7bba846b0
axelmaya69 <axeldbmaya96@gmail.com>
2025-07-07 22:23:23 -0600

    agregando security auth

---
+++ Auth-service/src/main/java/com/example/Auth_service/dtos/RegisterRequest.java
@@ -0,0 +1,10 @@
+package com.example.Auth_service.dtos;
+
+public class RegisterRequest {
+    private String username;
+    private String password;
+    private String nombre;
+    private String apellidoP;
+    private String apellidoM;
+    private String telefono;
+}
