52c504df85a1f97c735c2138e0c6044931707d22
axelmaya69 <axeldbmaya96@gmail.com>
2025-07-07 22:14:38 -0600

    agregando security auth

--- Auth-service/src/main/java/com/example/Auth_service/service/IServiceUser.java
+++ Auth-service/src/main/java/com/example/Auth_service/service/IServiceUser.java
@@ -1,12 +1,13 @@
 package com.example.Auth_service.service;
 
+import com.example.Auth_service.dtos.ClienteRequest;
 import com.example.Auth_service.entity.User;
 
 import java.util.List;
 
 public interface IServiceUser {
 
-    User registrarUser(String username, String password);
+    User registrarUser(String username, String password, ClienteRequest clienteReques);
 
     String logearUser(String username, String password);
 
