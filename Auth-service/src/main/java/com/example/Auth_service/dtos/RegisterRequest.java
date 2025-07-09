3dc40bbd5f0c195abd4d074b4a5f2fb988aac941
axelmaya69 <axeldbmaya96@gmail.com>
2025-07-08 22:09:58 -0600

    agregando security auth

--- Auth-service/src/main/java/com/example/Auth_service/dtos/RegisterRequest.java
+++ Auth-service/src/main/java/com/example/Auth_service/dtos/RegisterRequest.java
@@ -1,6 +1,16 @@
 package com.example.Auth_service.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+
+@Data
 public class RegisterRequest {
+
     private String username;
     private String password;
     private String nombre;
