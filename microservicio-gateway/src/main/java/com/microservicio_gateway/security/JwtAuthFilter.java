d63a0f1511100456eedede832e616c62d59ff2b2
axelmaya69 <axeldbmaya96@gmail.com>
2025-07-08 22:12:15 -0600

    agregando security auth

--- microservicio-gateway/src/main/java/com/microservicio_gateway/security/JwtAuthFilter.java
+++ microservicio-gateway/src/main/java/com/microservicio_gateway/security/JwtAuthFilter.java
@@ -1,4 +1,17 @@
 package com.microservicio_gateway.security;
 
-public class JwtAuthFilter {
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+public class JwtAuthFilter  implements GlobalFilter {
+
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return null;
+    }
 }
+
