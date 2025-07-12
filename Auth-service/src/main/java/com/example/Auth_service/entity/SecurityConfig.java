bd6fb1eb5cdce47fb1faba390d86a25a30a51f67
axelmaya69 <axeldbmaya96@gmail.com>
2025-07-11 22:19:38 -0600

    configurando seguridad

--- Auth-service/src/main/java/com/example/Auth_service/entity/SecurityConfig.java
+++ Auth-service/src/main/java/com/example/Auth_service/entity/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.example.Auth_service.entity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 public class SecurityConfig {
 
@@ -11,7 +13,17 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll() // dejas públicos /auth/login, /auth/register, etc.
+                        .anyRequest().authenticated()           // todo lo demás necesita auth
+                )
+                .formLogin(form -> form.disable()) // ❌ desactiva login por defecto
+                .build();
+    }
 
 
 }
