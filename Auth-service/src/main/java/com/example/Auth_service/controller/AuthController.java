5068f42d8c402c279b79886cdb903549a5cc1bf7
axelmaya69 <axeldbmaya96@gmail.com>
2025-07-16 21:54:01 -0600

    configurando seguridad

--- Auth-service/src/main/java/com/example/Auth_service/controller/AuthController.java
+++ Auth-service/src/main/java/com/example/Auth_service/controller/AuthController.java
@@ -1,21 +1,5 @@
 package com.example.Auth_service.controller;
 
-import com.example.Auth_service.dtos.ClienteRequest;
-import com.example.Auth_service.dtos.LoginRequest;
-import com.example.Auth_service.dtos.LoginResponse;
-import com.example.Auth_service.dtos.RegisterRequest;
-import com.example.Auth_service.entity.User;
-import com.example.Auth_service.service.IServiceUser;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/auth")
 
 
 }
