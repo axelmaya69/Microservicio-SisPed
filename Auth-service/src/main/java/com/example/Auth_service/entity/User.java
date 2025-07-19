1737ecf2d021da441c165771a7926615e2860dcd
axelmaya69 <axeldbmaya96@gmail.com>
2025-07-18 22:53:39 -0600

    configurando seguridad

--- Auth-service/src/main/java/com/example/Auth_service/entity/User.java
+++ Auth-service/src/main/java/com/example/Auth_service/entity/User.java
@@ -34,7 +34,7 @@ public class User implements UserDetails {
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority(rol.name()));
     }
-
+ 
     @Override
     public boolean isAccountNonExpired() {
         return true;
