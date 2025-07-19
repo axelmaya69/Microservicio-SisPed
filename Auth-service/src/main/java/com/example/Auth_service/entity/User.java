0274cee2028c598475bef65cf992bf27ea3a2219
axelmaya69 <axeldbmaya96@gmail.com>
2025-07-18 22:53:57 -0600

    configurando seguridad

--- Auth-service/src/main/java/com/example/Auth_service/entity/User.java
+++ Auth-service/src/main/java/com/example/Auth_service/entity/User.java
@@ -39,7 +39,6 @@ public class User implements UserDetails {
     public boolean isAccountNonExpired() {
         return true;
     }
-
     
     @Override
     public boolean isAccountNonLocked() {
