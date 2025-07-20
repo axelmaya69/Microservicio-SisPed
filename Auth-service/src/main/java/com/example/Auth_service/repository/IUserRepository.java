61b6d1376f67817c7fa6ff5495d54d1836556497
axelmaya69 <axeldbmaya96@gmail.com>
2025-07-19 22:29:00 -0600

    configurando seguridad

--- Auth-service/src/main/java/com/example/Auth_service/repository/IUserRepository.java
+++ Auth-service/src/main/java/com/example/Auth_service/repository/IUserRepository.java
@@ -3,8 +3,10 @@ package com.example.Auth_service.repository;
 import com.example.Auth_service.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IUserRepository extends JpaRepository<User, Long> {
 
-    User findByUsername(String username);
+    Optional <User> findByEmail(String email);
 
 }
