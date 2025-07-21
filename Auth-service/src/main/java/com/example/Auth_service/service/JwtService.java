8d93b4d9cb81706a35c72dfe042bbdc9ba85856b
axelmaya69 <axeldbmaya96@gmail.com>
2025-07-20 21:49:08 -0600

    configurando seguridad

--- Auth-service/src/main/java/com/example/Auth_service/service/JwtService.java
+++ Auth-service/src/main/java/com/example/Auth_service/service/JwtService.java
@@ -2,10 +2,12 @@ package com.example.Auth_service.service;
 
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
@@ -39,6 +41,11 @@ public class JwtService {
         return jwtExpiration;
     }
     private String buildToken(Map<String,Object> extraClaims, UserDetails userDetails, long expiration){
-        
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .
     }
 }
