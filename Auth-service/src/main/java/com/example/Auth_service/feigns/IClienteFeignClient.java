78c8c06f636210c83257a32bf748b110bd44920a
axelmaya69 <128741108+axelmaya69@users.noreply.github.com>
2025-07-22 22:27:57 -0600

    Actualizar IClienteFeignClient.java

--- Auth-service/src/main/java/com/example/Auth_service/feigns/IClienteFeignClient.java
+++ Auth-service/src/main/java/com/example/Auth_service/feigns/IClienteFeignClient.java
@@ -5,7 +5,6 @@ import com.example.Auth_service.dtos.ClienteResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 @FeignClient(name = "microservicio-cliente",url = "http://localhost:9080/api/cliente")
 public interface IClienteFeignClient {
 
