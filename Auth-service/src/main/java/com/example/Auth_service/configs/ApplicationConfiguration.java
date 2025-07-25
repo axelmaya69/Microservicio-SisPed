bafd124c41f6970db9a65ec16fc8106de1d499bb
axelmaya69 <axeldbmaya96@gmail.com>
2025-07-24 22:41:59 -0600

    configurando seguridad

--- Auth-service/src/main/java/com/example/Auth_service/configs/ApplicationConfiguration.java
+++ Auth-service/src/main/java/com/example/Auth_service/configs/ApplicationConfiguration.java
@@ -4,6 +4,11 @@ import com.example.Auth_service.repository.IUserRepository;
 import org.bouncycastle.crypto.generators.BCrypt;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -28,4 +33,19 @@ public class ApplicationConfiguration {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return authenticationProvider;
+
+    }
 }
