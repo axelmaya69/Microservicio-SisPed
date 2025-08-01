848bfb03dc29384c4cfe9fb4a3fd5b1fc0f7c52e
axelmaya69 <axeldbmaya96@gmail.com>
2025-07-31 22:42:45 -0600

    añadiendo seguridad

---
+++ Auth-service/src/main/java/com/example/Auth_service/configs/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.example.Auth_service.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.HttpSecurityDsl;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    private final AuthenticationProvider authenticationProvider;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+
+    public SecurityConfiguration(AuthenticationProvider authenticationProvider, JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.authenticationProvider = authenticationProvider;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
+        
+    }
+}
