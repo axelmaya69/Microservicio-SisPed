5adfe6aee6d60fac8dde6be59a8818b4aec765cf
axelmaya69 <axeldbmaya96@gmail.com>
2025-08-03 22:25:32 -0600

    añadiendo seguridad

---
+++ Auth-service/src/main/java/com/example/Auth_service/controller/AuthenticationController.java
@@ -0,0 +1,13 @@
+package com.example.Auth_service.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/auth")
+@RestController
+public class AuthenticationController {
+
+    
+
+}
