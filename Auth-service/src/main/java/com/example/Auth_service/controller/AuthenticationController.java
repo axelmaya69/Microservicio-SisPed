5e876befca2bcd8cadf5dfdfaf1c831e7dfe9aae
axelmaya69 <axeldbmaya96@gmail.com>
2025-08-04 22:32:13 -0600

    añadiendo seguridad

--- Auth-service/src/main/java/com/example/Auth_service/controller/AuthenticationController.java
+++ Auth-service/src/main/java/com/example/Auth_service/controller/AuthenticationController.java
@@ -1,6 +1,13 @@
 package com.example.Auth_service.controller;
 
 
+import com.example.Auth_service.dtos.RegisterRequest;
+import com.example.Auth_service.entity.User;
+import com.example.Auth_service.service.AuthenticationService;
+import com.example.Auth_service.service.JwtService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthenticationController {
 
-    
+    private final JwtService jwtService;
 
+    private final AuthenticationService authenticationService;
+
+
+    public AuthenticationController(JwtService jwtService, AuthenticationService authenticationService) {
+        this.jwtService = jwtService;
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> register (@RequestBody RegisterRequest registerRequest){
+        User registerUser = authenticationService.signup(registerRequest);
+
+        return ResponseEntity.ok(registerUser);
+    }
 }
