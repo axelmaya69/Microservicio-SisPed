e9c44fc8e2298f3bea4a8ff91e783eb001020659
axelmaya69 <axeldbmaya96@gmail.com>
2025-08-11 22:01:15 -0600

    añadiendo seguridad

--- microservicio-gateway/src/main/java/com/microservicio_gateway/JwtUtil.java
+++ microservicio-gateway/src/main/java/com/microservicio_gateway/JwtUtil.java
@@ -1,4 +1,9 @@
 package com.microservicio_gateway;
 
 public class JwtUtil {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public boolean validateToken(String token) {
+        try {
 }
