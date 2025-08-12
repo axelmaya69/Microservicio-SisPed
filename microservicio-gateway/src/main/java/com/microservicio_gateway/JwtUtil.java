8e9015a28718bebdd143c1aec0e6959b03728924
axelmaya69 <axeldbmaya96@gmail.com>
2025-08-11 22:01:40 -0600

    añadiendo seguridad

--- microservicio-gateway/src/main/java/com/microservicio_gateway/JwtUtil.java
+++ microservicio-gateway/src/main/java/com/microservicio_gateway/JwtUtil.java
@@ -1,9 +1,21 @@
 package com.microservicio_gateway;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class JwtUtil {
     @Value("${jwt.secret}")
     private String secret;
 
     public boolean validateToken(String token) {
         try {
+
+            Jwts.parser()
+                    .setSigningKey(secret)
+                    .parseClaimsJws(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
 }
