8604cf377b03487a2e80ff48a182eca32a3241f4
axelmaya69 <128741108+axelmaya69@users.noreply.github.com>
2025-10-09 22:08:00 -0600

    Actualizar User.java

--- Auth-service/src/main/java/com/example/Auth_service/entity/User.java
+++ Auth-service/src/main/java/com/example/Auth_service/entity/User.java
@@ -46,7 +46,6 @@ public class User implements UserDetails {
     @Column(name = "updated_at")
     private Date updatedAt;
 
-
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of();
