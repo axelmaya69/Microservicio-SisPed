73fc4cb89d9042496d6332a9d322f54a998a3bba
axelmaya69 <axeldbmaya96@gmail.com>
2025-10-14 22:32:08 -0600

    commmit 6

--- Auth-service/src/main/java/com/example/Auth_service/configs/SecurityConfiguration.java
+++ Auth-service/src/main/java/com/example/Auth_service/configs/SecurityConfiguration.java
@@ -28,7 +28,6 @@ public class SecurityConfiguration {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
 
-
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
         http.authorizeHttpRequests( authorizeRequest ->
